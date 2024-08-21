bbaf50b6b0f39149fa317d5a2d39a5aab0ca7c73
khomotso gojela <motsoak49@gmail.com>
2024-08-21 09:49:39 +0200

    chapter6 update

--- classwork/chapter6/server.js
+++ classwork/chapter6/server.js
@@ -11,7 +11,52 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+// Handle GET request for profile edit
+app.get("/user/:id/edit", (req, res) => {
+  // send an HTML form for editing
+  res.send(/*html*/`
+  <form hx-put="/user/1" hx-target="this" hx-swap="outerHTML">
+  <div class="mb-3">
+  <label for="name" class="form-label">Name</label>
+  <input type="text" class="form-control" id="name" name="name" value="Greg
+ Lim">
+  </div>
+  <div class="mb-3">
+  <label for="bio" class="form-label">Bio</label>
+  <textarea type="text" class="form-control" id="bio" name="bio">Follower of Christ |
+ Author of Best-selling Amazon Tech Books and Creator of Coding Courses
+  </textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">
+Save Changes
+ </button>
+ </form>
+ `);
+});
 
+// Handle PUT request for editing
+app.put("/user/:id", (req, res) => {
+  const name = req.body.name;
+  const bio = req.body.bio;
+  // Send the updated profile back
+  res.send(/*html*/ `
+  <div class="card" style="width: 18rem;"
+  hx-target="this"
+  hx-swap="outerHTML"
+  >
+    <div class="card-body">
+      <h5 class="card-title">${name}</h5>
+      <p class="card-text">
+          ${bio}
+      </p>
+      <button href="#" class="btn btn-primary"
+        hx-get="/user/1/edit">
+          Click To Edit
+      </button>
+    </div>
+  </div>
+  `);
+});
 
 // Start the server
 app.listen(3000, () => {
